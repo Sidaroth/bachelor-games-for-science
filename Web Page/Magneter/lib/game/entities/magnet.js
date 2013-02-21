b53ab02d643c5bea9966bb89d9fb34de6bddca70
Christian Holt <ymabob@gmail.com>
2013-02-21 15:22:57 +0100

    Started working on implementing the decay function and field effect radius drawing

--- Web Page/Magneter/lib/game/entities/magnet.js
+++ Web Page/Magneter/lib/game/entities/magnet.js
@@ -15,9 +15,10 @@ EntityMagnet = ig.Entity.extend({
 	size: {x: 10, y: 10},
 	//offset: {x: 0, y:0 },
 
-	fieldRadius: 0,		// Radius of the circle the magnet will have an effect on.
-	fieldStrength: 10,  // The strenght of the magnetic field (Used to calculate the strength at a location)
+	fieldRadius: 50,		// Radius of the circle the magnet will have an effect on.
+	fieldStrength: 10,  // The strength of the magnetic field (Used to calculate the strength at a location)
 	polarity: 0,		// Polarities are represented as (1, 0, -1)(North, Neutral, South). 
+	ringColor: 'rgba(255, 255, 255, 1)',
 
 	player: null,
 
@@ -26,15 +27,64 @@ EntityMagnet = ig.Entity.extend({
 		this.player = ig.game.getEntitiesByType(EntityPlayer)[0];
 	},
 
-	update: function()
+
+	handleEvents: function()
 	{
 		if(this.player === null)
 		{
 			this.player = ig.game.getEntitiesByType(EntityPlayer)[0];
 		}
 
+		if(ig.input.pressed('right'))
+		{
+			this.pos.x += 10;
+		}
+
+		if(ig.input.pressed('left'))
+		{
+			this.pos.x -= 10;
+		}
+
+		if(ig.input.pressed('up'))
+		{
+			this.pos.y -= 10;
+		}
+
+		if(ig.input.pressed('down'))
+		{
+			this.pos.y += 10;
+		}
+
+		console.log(this.pos);
+	},
+
+	update: function()
+	{
+
+		this.handleEvents();
+		
 		//console.log("ig.game: " + ig.game.getEntitiesByType(EntityPlayer)[0].pos.x);
 		this.parent();
+	},
+
+	draw: function()
+	{
+		this.parent();
+		this.drawFieldEffectRing();
+	},
+
+	drawFieldEffectRing: function()
+	{
+		var context = ig.system.context;
+
+		context.strokeStyle = this.ringColor; 
+		context.beginPath();
+		context.arc( ig.system.getDrawPos( this.pos.x - ig.game.screen.x ),
+					ig.system.getDrawPos( this.pos.y - ig.game.screen.y ),
+					this.fieldRadius * ig.system.scale,
+					0, Math.PI * 2);
+
+		context.stroke();
 	}
 })
 ;});
