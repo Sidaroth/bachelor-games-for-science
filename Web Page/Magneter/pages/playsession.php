bb6708c1ecb56b303f223b20669ee793fd050d1e
Øyvind Nordli <oe.nordli@gmail.com>
2013-05-12 20:55:11 +0200

    php stuff

--- Web Page/Magneter/pages/playsession.php
+++ Web Page/Magneter/pages/playsession.php
@@ -2,15 +2,10 @@
 
 <?php 
 	if(isset($_POST['metricMD5']) && isset($_POST['playMD5'])){
-		$metricMD5 = $_POST['metricMD5'];
-		$playMD5 = $_POST['playMD5'];
 		include('connect.php');
-	    $sql = 'INSERT INTO playsession( playMD5, metricMD5)
-	    		VALUES (:playMD5, :metricMD5)';
-	    $sth = $db->prepare($sql);
-	    $sth->bindParam (':metricMD5', $metricMD5);
-	    $sth->bindParam (':playMD5', $playMD5);
-	    $sth->execute();
+	    $sth = $db->prepare('	INSERT INTO playsession( playMD5, metricMD5)
+	    						VALUES (?, ?)');
+	    $sth->execute(array($_POST['playMD5'], $_POST['metricMD5']));
 	    $sth->closeCursor();
 	}
 ?>
